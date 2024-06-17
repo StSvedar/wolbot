07798bd5ecc3355d00ff5d69f6e840bdeca437f0
malog <malo.goossens@outlook.com>
2024-06-17 22:02:47 +0200

    feat: shutdown command

--- Main.py
+++ Main.py
@@ -25,28 +25,47 @@ async def ping(ctx):
     bot.log("Ping command")
     await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 
+@bot.command() # Check target status command
+async def isup(ctx):
+    bot.log(f"Check status command on {ipv4}")
+    await ctx.send("Checking target status...")
+    status = "up" if check_status(ipv4) else "down"
+    bot.log(f"{ipv4} is {status}")
+    await ctx.send(f"Target is {status}")
+
 @bot.command() # Wake up command
 async def wakeitup(ctx):
     bot.log(f"Wake up command on {mac_address}")
     bot.log(f"Check status of {ipv4}")
-    await ctx.send("Checking status of the target...")
+    await ctx.send("Checking target status...")
 
     if check_status(ipv4):
         bot.log("The target is already up")
         await ctx.send("The target is already up")
         return
-    wake_on_lan(mac_address)
+    if wake_on_lan(mac_address):
+        bot.log("Magic packet sent")
+        await ctx.send("Waking up the computer")
+    else:
+        bot.log("Failed to send magic packet")
+        await ctx.send("Failed to wake up the computer")
 
-    bot.log("Magic packet sent")
-    await ctx.send("Waking up the computer")
+@bot.command() # Shutdown command
+async def shutitdown(ctx):
+    bot.log(f"Shutdown command on {ipv4}")
+    bot.log(f"Check status of {ipv4}")
+    await ctx.send("Checking target status...")
 
-@bot.command() # Check target status command
-async def isup(ctx):
-    bot.log(f"Check status command on {ipv4}")
-    await ctx.send("Checking status of the target...")
-    status = "up" if check_status(ipv4) else "down"
-    bot.log(f"{ipv4} is {status}")
-    await ctx.send(f"Target is {status}")
+    if not check_status(ipv4):
+        bot.log("The target is already down")
+        await ctx.send("The target is already down")
+        return
+    if windows_shutdown(ipv4, user, password):
+        bot.log("Shutdown command sent")
+        await ctx.send("Shutting down the computer")
+    else:
+        bot.log("Failed to send shutdown command")
+        await ctx.send("Failed to shut down the computer")
 
 @bot.command() # Close command
 async def close(ctx):
--- Utils/Shell_manager.py
+++ Utils/Shell_manager.py
@@ -1,9 +1,14 @@
 import os
 import subprocess
 
-def check_status(host):
-    response = subprocess.run(f"ping -c 1 {host}", stdout=subprocess.PIPE, shell=True)
+def check_status(host, delay=2):
+    response = subprocess.run(f"ping -w {delay} {host}", stdout=subprocess.PIPE, shell=True)
     return response.returncode == 0
 
 def wake_on_lan(mac_address):
-    os.system(f"wakeonlan {mac_address}")
+    response = subprocess.run(f"wakeonlan {mac_address}", shell=True)
+    return response.returncode == 0
+
+def windows_shutdown(host, user, password, delay=0):
+    response = subprocess.run(f"net rpc -S {host} -U {user}%{password} shutdown -f -t {delay}", shell=True)
+    return response.returncode == 0
