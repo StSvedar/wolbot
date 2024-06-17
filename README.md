d9d631d9b1b1483a9ba222b3686ef6518bfeb3fa
malog <malo.goossens@outlook.com>
2024-06-17 22:15:11 +0200

    fix: sutdown default delay

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 Fancy explanation
 
 requirements :
+for linux server :
 installing discord library "sudo pip install discord.py"
-installing wakeonlan "sudo apt-get wakeonlan"
+installing wakeonlan "sudo apt-get install wakeonlan"
+samba
+
+for windows target :
 if targeted computer running on windows : allow pings in the firewall
+
+for linux target :
+... ?
--- Utils/Shell_manager.py
+++ Utils/Shell_manager.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 def check_status(host, delay=2):
@@ -9,6 +8,6 @@ def wake_on_lan(mac_address):
     response = subprocess.run(f"wakeonlan {mac_address}", shell=True)
     return response.returncode == 0
 
-def windows_shutdown(host, user, password, delay=0):
+def windows_shutdown(host, user, password, delay=1):
     response = subprocess.run(f"net rpc -S {host} -U {user}%{password} shutdown -f -t {delay}", shell=True)
     return response.returncode == 0
